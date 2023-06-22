af144b56f515756a984924bee6ad3e02bae6a919
Stefan Borek <sztefan.borek@gmail.com>
2023-06-22 23:43:29 +0200

    improves etl script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Bike sharing in Chicago
-* BI ETL analysis
+# Bike sharing w Chicago
+
+ETL i tworzenie tabel odbywa się w [scripts/etl.py](https://github.com/DamianZawolski/BIProject/blob/main/scripts/etl.py).
--- scripts/etl.py
+++ scripts/etl.py
@@ -20,7 +20,7 @@ RENTAL_COLUMNS = ['Ride_Id', 'Rideable_Type', 'Started_At', 'Ended_At',
 WEATHER_COLUMNS = ['time', 'temperature_2m (°C)', 'precipitation (mm)', 'cloudcover (%)',
        'windspeed_10m (km/h)', 'latitude', 'longitude', 'elevation',
        'utc_offset_seconds', 'timezone', 'timezone_abbreviation']
-SAVE_DATA_TO = 'data'
+SAVE_DATA_TO = os.path.join('..', 'etl_tables')
 
 
 def load_rentals_data(filename=BIKE_RENTALS_FILE, data_folder=DATA_FOLDER):
@@ -41,19 +41,70 @@ def check_columns(df, columns):
         return True
     else:
         return False
-    
+
+
+
+def create_type_table(df) -> dict:
+    i = 0
+    content = {'Type_Id': [], 'Rideable_Type': [], 'Member_Casual': [], 'Type_Id_str': []}
+    for bike_type in  df['Rideable_Type'].unique():
+        for member_type in df['Member_Casual'].unique():
+            content['Type_Id'].append(i)
+            content['Rideable_Type'].append(bike_type)
+            content['Member_Casual'].append(member_type)
+            content['Type_Id_str'].append(bike_type + '_' + member_type)
+            i += 1
+
+    return content
+
+def set_rental_type_id(rideable_type, member_type):
+    if rideable_type == 'docked_bike':
+        if member_type == 'casual':
+            return 0
+        else:
+            return 1
+    elif rideable_type == 'electric_bike':
+        if member_type == 'casual':
+            return 2
+        else:
+            return 3
+    elif rideable_type == 'classic_bike':
+        if member_type == 'casual':
+            return 4
+        else:
+            return 5
+
+
+
+def create_rental_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create a dataframe with rental data
+
+    :param df: dataframe with rental data
+    :return: dataframe with rental data
+    """
+    df_rentals = df[['Ride_Id', 'Started_At', 'Ended_At', 'Start_Station_Id', 'End_Station_Id', 'Rideable_Type', 'Member_Casual']].copy()
+    df_rentals['Started_At'] = pd.to_datetime(df_rentals['Started_At'], format='%m/%d/%Y %H:%M')
+    df_rentals['Ended_At'] = pd.to_datetime(df_rentals['Ended_At'], format='%m/%d/%Y %H:%M')
+    df_rentals['Date'] = df_rentals['Started_At'].dt.date
+    df_rentals['Ride_Length'] = (df_rentals['Ended_At'] - df_rentals['Started_At']).dt.total_seconds() // 60
+    df_rentals['Ride_Length'] = df_rentals['Ride_Length'].astype(int)
+    df_rentals['Type_Id'] = df_rentals.apply(lambda x: set_rental_type_id(x['Rideable_Type'], x['Member_Casual']), axis=1)
+    df_rentals = df_rentals[['Ride_Id', 'Date', 'Start_Station_Id', 'End_Station_Id', 'Ride_Length', 'Started_At', 'Type_Id']]
+    df_rentals.dropna(inplace=True)
+    return df_rentals
+
 
 def create_time_df(df: pd.DataFrame) -> pd.DataFrame:
-    df_time = df[['Ride_Id', 'Ride_Length', 'Started_At', 'Ended_At', 'Day']].copy()
-
-    df_time['Started_At'] = pd.to_datetime(df_time['Started_At'], format='%m/%d/%Y %H:%M')
+    df_time = df[['Ride_Id', 'Started_At', 'Ended_At', 'Day']].copy()
     df_time['Ended_At'] = pd.to_datetime(df_time['Ended_At'], format='%m/%d/%Y %H:%M')
+    df_time['Started_At'] = pd.to_datetime(df_time['Started_At'], format='%m/%d/%Y %H:%M', errors='coerce')
+    df_time.dropna(inplace=True)
     df_time['Date'] = df_time['Started_At'].dt.date
-    df_time['Duration'] = (df_time['Ended_At'] - df_time['Started_At']).dt.total_seconds() // 60
-    df_time['Duration'].astype(int)
+    df_time['Time_Of_The_Day'] = df_time['Started_At'].dt.hour
     df_time['Time_Of_The_Day'] = df_time['Started_At'].dt.hour.apply(remap_time)
 
-    df_time = df_time[['Ride_Id', 'Ride_Length', 'Duration', 'Time_Of_The_Day', 'Day', 'Date']]
+    df_time = df_time[['Started_At', 'Time_Of_The_Day', 'Day']]
     df_time.dropna(inplace=True)
     return df_time
 
@@ -82,6 +133,29 @@ def create_weather_df(df: pd.DataFrame) -> pd.DataFrame:
     return df_weather
 
 
+
+
+
+def create_location_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create a dataframe with location data, merges start and end station location data
+    returns unique station data
+    :param df: dataframe with rental data
+    :return: dataframe with location data
+    """
+    start_df = df[['Start_Station_Id', 'Start_Station_Name', 'Start_Lat', 'Start_Lng']].copy()
+    start_df.columns = ['Station_Id', 'Station_Name', 'Lat', 'Lng']
+
+    end_df = df[['End_Station_Id', 'End_Station_Name', 'End_Lat', 'End_Lng']].copy()
+    end_df.columns = ['Station_Id', 'Station_Name', 'Lat', 'Lng']
+
+    df_stations = pd.concat([start_df, end_df])
+    df_stations = df_stations.drop_duplicates(subset=['Station_Id'])
+    df_stations.dropna(inplace=True)
+    logger.info('station df create succesfully.')
+    return df_stations
+
+
 def main():
     bike_rentals = load_rentals_data(BIKE_RENTALS_FILE)
     weather = load_weather_data(WEATHER_FILE)
@@ -96,52 +170,29 @@ def main():
         sys.exit(1)
 
 
-    # creating rentals table
-    df_rentals = bike_rentals[['Ride_Id', 'Started_At', 'Ended_At']].copy()
-    df_rentals['Started_At'] = pd.to_datetime(df_rentals['Started_At'], format='%m/%d/%Y %H:%M')
-    df_rentals['Ended_At'] = pd.to_datetime(df_rentals['Ended_At'], format='%m/%d/%Y %H:%M')
-    df_rentals['Date'] = df_rentals['Started_At'].dt.date
-
-    # creatin time table
+    df_rentals = create_rental_df(bike_rentals)
+    logger.info("Rental dataframe created successfully")
     df_time = create_time_df(bike_rentals)
     logger.info("Time dataframe created successfully")
-
-    # creating location table
-    df_location = bike_rentals[['Ride_Id', 'Start_Station_Id', 'End_Station_Id']].copy()
-    df_location.dropna(inplace=True)
-
-    # creating stations table
-    df_stations = bike_rentals[['Start_Station_Id', 'Start_Station_Name', 'Start_Lat', 'Start_Lng']].copy()
-    df_stations = df_stations.drop_duplicates(subset=['Start_Station_Id'])
-    df_stations.dropna(inplace=True)
+    df_stations = create_location_df(bike_rentals)
+    logger.info("Location dataframe created successfully")
 
     # creating types table
-    df_types = bike_rentals[['Ride_Id', 'Rideable_Type', 'Member_Casual']].copy()
-    df_types.dropna(inplace=True)
+    df_types = pd.DataFrame(create_type_table(bike_rentals))
 
     df_weather = create_weather_df(weather)
     logger.info("Weather dataframe created successfully")
 
-    # saveing all df to csv files    
+    # saving all df to csv files    
+    logger.info('Saving data...')
     os.makedirs(SAVE_DATA_TO, exist_ok=True)
+    df_rentals.to_csv(os.path.join(SAVE_DATA_TO, 'rentals.csv'), index=False)
     df_time.to_csv(os.path.join(SAVE_DATA_TO, 'time.csv'), index=False)
-    df_location.to_csv(os.path.join(SAVE_DATA_TO, 'location.csv'), index=False)
     df_stations.to_csv(os.path.join(SAVE_DATA_TO, 'stations.csv'), index=False)
     df_types.to_csv(os.path.join(SAVE_DATA_TO, 'types.csv'), index=False)
     df_weather.to_csv(os.path.join(SAVE_DATA_TO, 'weather.csv'), index=False)
     logger.info("Dataframes saved successfully")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='CSV Parser')
-    parser.add_argument('-d', '--data_folder', help='Data folder path', default=None)
-    parser.add_argument('-r', '--rentals', help='CSV file path for rentals')
-    parser.add_argument('-w', '--weather', help='CSV file path for weather')
-    args = parser.parse_args()
-
-    if args.data_folder:
-        BIKE_RENTALS_FILE = args.rentals
-    if args.data_folder:
-        WEATHER_FILE = args.weather
-    
+if __name__ == "__main__":  
     main()
